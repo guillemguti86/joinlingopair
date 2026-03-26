989adb23d134465262bd857ebf20ae837d8e4008
guillemguti86 <guillemguti@gmail.com>
2026-03-26 17:11:59 +0100

    Add internationalization support for Spanish, English, and French translations

---
+++ public/js/i18n.js
@@ -0,0 +1,19 @@
+const i18n = {
+    en: {
+        welcome: "Welcome",
+        goodbye: "Goodbye",
+        thankYou: "Thank you"
+    },
+    es: {
+        welcome: "Bienvenido",
+        goodbye: "Adiós",
+        thankYou: "Gracias"
+    },
+    fr: {
+        welcome: "Bienvenue",
+        goodbye: "Au revoir",
+        thankYou: "Merci"
+    }
+};
+
+export default i18n;
